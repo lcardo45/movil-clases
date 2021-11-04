5633a2c6a27c697c81de62a56896f1b48a9acc5b
lcardo45 <felipe2010@hotmail.es>
2021-11-04 15:56:03 -0500

    Vinculación de ventanas

--- app/src/main/java/com/misiontic/holamundo01/LoginActivity.java
+++ app/src/main/java/com/misiontic/holamundo01/LoginActivity.java
@@ -1,13 +1,28 @@
 package com.misiontic.holamundo01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToMain(View view){
+        etName = (EditText) findViewById(R.id.editTextTextPersonName);
+        String name = etName.getText().toString();
+
+        Intent intentMain = new Intent(this, MainActivity.class);
+
+        intentMain.putExtra("user", name);
+
+        startActivity(intentMain);
     }
 }
--- app/src/main/java/com/misiontic/holamundo01/MainActivity.java
+++ app/src/main/java/com/misiontic/holamundo01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.misiontic.holamundo01;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String name = getIntent().getStringExtra("user");
+        Toast.makeText(this,"Bienvenido(a) " + name, Toast.LENGTH_LONG).show();
+    }
+
+    public void goToLogin(View view) {
+        Intent intentLogin = new Intent(this, LoginActivity.class);
+        startActivity(intentLogin);
     }
 }
